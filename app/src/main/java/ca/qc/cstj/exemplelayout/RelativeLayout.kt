503060e373d49b22fd20679da9b86805381afbec
Jean-Philippe Lalonde <1030469@cstj.qc.ca>
2018-11-19 14:58:49 -0500

    firstone

---
+++ app/src/main/java/ca/qc/cstj/exemplelayout/RelativeLayout.kt
@@ -0,0 +1,12 @@
+package ca.qc.cstj.exemplelayout
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RelativeLayout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_layout2)
+    }
+}
